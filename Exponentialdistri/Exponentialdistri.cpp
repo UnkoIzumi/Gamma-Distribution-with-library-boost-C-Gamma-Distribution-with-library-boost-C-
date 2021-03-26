e8f7942da6c8484ea520c046ef445581033698e8
UnkoIzumi <46561696+UnkoIzumi@users.noreply.github.com>
2021-03-26 15:30:46 +0700

    Update Exponentialdistri.cpp

--- Exponentialdistri/Exponentialdistri.cpp
+++ Exponentialdistri/Exponentialdistri.cpp
@@ -1,16 +1,17 @@
 // Exponentialdistri.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
+#include <iostream> // input output program
+#include <iomanip> // input output manipuasi
+#include <cmath> // matematic logika
+#include <cstdlib> // system pause and clear
 using namespace std;
 
-class exponen {
+class exponent {
 private :
 	const double e = 2.71828;
 	double X = 0, lamda = 0, X1 = 0, X2 = 0, F = 0, F1 = 0,  F2 =0;
-	double phi, persen, P, P1, P2;
+	double phi = 0, persen = 0, P = 0, P1 = 0, P2 = 0;
 public:
 	void kurangdari() {
 		cout << " == Proram Kurang Dari == " << endl;
@@ -21,8 +22,8 @@ public:
 		F = -phi * X;
 		P = 1 - pow(e, F);
 		persen = abs(P) * 100;
-		cout << "Probabilitasnya adalah " << fixed << setprecision(3) << abs(P) << endl;
-		cout << "Dengan Persen : " << persen << "%" << endl;
+		cout << "\nProbabilitasnya adalah " << fixed << setprecision(3) << abs(P) << endl;
+		cout << "Dengan Persen : " << fixed << setprecision(2) << persen << "%" << endl << endl;
 	}
 
 	void lebihdari() {
@@ -34,8 +35,8 @@ public:
 		F = -phi * X;
 		P = pow(e, F);
 		persen = abs(P) * 100;
-		cout << "Probabilitasnya adalah " << fixed << setprecision(3) << abs(P) << endl;
-		cout << "Dengan Persen : " << persen << "%" << endl;
+		cout << "\nProbabilitasnya adalah " << fixed << setprecision(3) << abs(P) << endl;
+		cout << "Dengan Persen : " << fixed << setprecision(2) << persen << "%" << endl << endl;
 	}
 
 	void diantara() {
@@ -51,11 +52,60 @@ public:
 		P2 = 1 - pow(e, F2);
 		P = P1 - P2;
 		persen = abs(P) * 100;
-		cout << "Probabilitasnya adalah " << fixed << setprecision(3) << abs(P) << endl;
-		cout << "Dengan Persen : " << persen << "%" << endl;
+		cout << "\nProbabilitasnya adalah " << fixed << setprecision(3) << abs(P) << endl;
+		cout << "Dengan Persen : " << fixed << setprecision(2) << persen << "%" << endl << endl;
+	}
+
+	int menu() {
+		int pilih;
+		system("cls");
+		cout << " ___ Exponential Distribution ___ " << endl;
+		cout << " 1. Nilai Diantara    " << endl;
+		cout << " 2. Nilai Kurang Dari " << endl;
+		cout << " 3. Nilai Lebih Dari  " << endl;
+		cout << " 4. Keluar            " << endl;
+		cout << "______________________" << endl;
+		cout << " Pilihan : "; cin >> pilih;
+
+		return pilih;
 	}
 };
 
 int main() {
+	exponent Demo;
+	int user;
 
+	do {
+		user = Demo.menu();
+		switch (user) {
+		case 1:
+			system("cls");
+			Demo.diantara();
+			system("pause");
+			break;
+		case 2:
+			system("cls");
+			Demo.kurangdari();
+			system("pause");
+			break;
+		case 3:
+			system("cls");
+			Demo.lebihdari();
+			system("pause");
+			break;
+		case 4:
+			system("cls");
+			break;
+		default:
+			if (!user) {
+				cout << "\n--Pilihan anda tidak ditemukan--" << endl;
+				cin.clear();
+				cin.ignore();
+			}
+			system("cls");
+			break;
+		}
+	} while (user != 4);
+	
+	return 0;
 }
